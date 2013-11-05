fd78371cb5129fe005242294c7c8c0b9cfbcd21a
cafferidwan <quazi.hasib13@gmail.com>
2013-11-05 20:46:22 +0600

    2nd commit

--- src/com/example/tutorial/Functions.java
+++ src/com/example/tutorial/Functions.java
@@ -3,14 +3,22 @@ package com.example.tutorial;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.DelayModifier;
 import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.modifier.PathModifier;
 import org.andengine.entity.modifier.SequenceEntityModifier;
 import org.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
+import org.andengine.entity.modifier.PathModifier.IPathModifierListener;
+import org.andengine.entity.modifier.PathModifier.Path;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.util.modifier.IModifier;
+import org.andengine.util.modifier.ease.EaseSineInOut;
+import android.media.MediaPlayer;
 
 public class Functions 
 {
 
+	static Boolean audioPlay = false;
+	static MediaPlayer mediaPlayer = new MediaPlayer();
+	
 	static void parrotWithLetterPath(final int number, int flip, float time, Sprite sprite, Sprite sprite1, float x1, final float x2, float y1, float y2) 
 	{
 		
@@ -26,7 +34,7 @@ public class Functions
 		MoveModifier mModParrot = new MoveModifier(time, x1, x2, y1, y2);
 		MoveModifier mModLetter = new MoveModifier(time, x1 +20, x2 +20, y1 +120, y2 +120);
 		
-		DelayModifier dMod = new DelayModifier((float) time,new IEntityModifierListener()
+		DelayModifier dMod = new DelayModifier((float) 1,new IEntityModifierListener()
 		{
 
 					@Override
@@ -41,6 +49,34 @@ public class Functions
 							IEntity arg1)
 					{
 						MainActivity.parrotWay = number;
+						
+						if(MainActivity.parrotWay == 10)
+						{
+							Functions.letter(11, 1, MainActivity.letter4, MainActivity.letter4Pos, 
+									MainActivity.letter4Pos,
+									MainActivity.CAMERA_HEIGHT / 2 -80, 
+									MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+						}
+						else if(MainActivity.parrotWay == 7)
+						{
+							Functions.letter(8, 1, MainActivity.letter3, MainActivity.letter3Pos, 
+									MainActivity.letter3Pos,MainActivity.CAMERA_HEIGHT / 2 -80, 
+									MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+						}
+						else if(MainActivity.parrotWay == 4)
+						{
+							Functions.letter(5, 2, MainActivity.letter2, MainActivity.letter2Pos, 
+									MainActivity.letter2Pos,
+									MainActivity.CAMERA_HEIGHT / 2 -80, 
+									MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+						}
+						else
+						{
+							Functions.letter(2, 1, MainActivity.letter1, MainActivity.letter1Pos +20, 
+									MainActivity.letter1Pos +20,
+									MainActivity.CAMERA_HEIGHT / 2 -80, 
+									MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+						}
 					}
 				});
 		
@@ -49,13 +85,11 @@ public class Functions
 		
 		sprite1.registerEntityModifier(parrot_sm);
 		sprite.registerEntityModifier(mLetter_sm);
-		
 	}
 	
 	static void letter(final int number, float time,final Sprite sprite, float x1, float x2, float y1, float y2)
 	{
 		MoveModifier mModLetter1 = new MoveModifier(time, x1, x2, y1, y2);
-		
 		DelayModifier dMod1 = new DelayModifier((float) time,new IEntityModifierListener()
 		{
 
@@ -65,12 +99,48 @@ public class Functions
 					{
 					
 					}
-
+					
 					@Override
 					public void onModifierFinished(IModifier<IEntity> arg0,
 							IEntity arg1)
 					{
-						MainActivity.parrotWay = number; 
+						MainActivity.parrotWay = number;
+						
+						if(MainActivity.parrotWay == 11)
+						{
+							MainActivity.parrot.setFlippedHorizontal(true);
+							
+							Functions.path(12, MainActivity.parrot, MainActivity.letter4Pos,
+									MainActivity.parrotHeight,
+									MainActivity.letter1Pos, MainActivity.letter1.getY(),
+									-200, MainActivity.parrotHeight
+									);
+						}
+						else if(MainActivity.parrotWay == 8)
+						{
+							Functions.parrotPath(9, 4, MainActivity.parrot, 
+									MainActivity.letter3Pos, -400,
+									MainActivity.parrotHeight,
+									MainActivity.parrotHeight
+									);
+						}
+						else if(MainActivity.parrotWay == 5)
+						{
+							Functions.parrotPath(6, 3, MainActivity.parrot, MainActivity.letter2Pos,
+									MainActivity.CAMERA_WIDTH+400,
+									MainActivity.parrotHeight,
+									MainActivity.parrotHeight
+									);
+						}
+						else
+						{
+						
+						Functions.parrotPath(3, 2, MainActivity.parrot, MainActivity.letter1Pos,
+								-400, 
+								MainActivity.parrotHeight,
+								MainActivity.parrotHeight
+								);
+						}
 					}
 				});  
 		
@@ -78,11 +148,11 @@ public class Functions
 		sprite.registerEntityModifier(mLetter_sm);
 	}
 	
+	
 	protected static void parrotPath(final int number, float time, Sprite sprite, float x1, float x2, float y1, float y2)
 	{
 		MoveModifier mModParrot1 = new MoveModifier(time, x1, x2, y1, y2);
-		
-		DelayModifier dMod1 = new DelayModifier((float) time,new IEntityModifierListener()
+		DelayModifier dMod1 = new DelayModifier((float) 0,new IEntityModifierListener()
 		{
 
 					@Override
@@ -96,12 +166,119 @@ public class Functions
 					public void onModifierFinished(IModifier<IEntity> arg0,
 							IEntity arg1)
 					{
-						
 						MainActivity.parrotWay = number; 
+						
+						if(MainActivity.parrotWay == 9)
+						{
+							Functions.parrotWithLetterPath(10,
+									1, 7, MainActivity.letter4, MainActivity.parrot, -600, 
+									MainActivity.letter4Pos, 
+									MainActivity.parrotHeight, MainActivity.parrotHeight);
+						}
+						else if(MainActivity.parrotWay == 6)
+						{
+							Functions.parrotWithLetterPath(7, 
+									0, 4, MainActivity.letter3, MainActivity.parrot, 
+									MainActivity.CAMERA_WIDTH+400, MainActivity.letter3Pos, 
+									MainActivity.parrotHeight, MainActivity.parrotHeight);
+						} 
+						else
+						{
+						Functions.parrotWithLetterPath(4, 
+								1, 4, MainActivity.letter2, MainActivity.parrot, -400, 
+								MainActivity.letter2Pos,
+								MainActivity.parrotHeight, MainActivity.parrotHeight);
+					
+						}
 					}
 				});
 		
 		SequenceEntityModifier mLetter_sm1 = new SequenceEntityModifier(mModParrot1,dMod1);
 		sprite.registerEntityModifier(mLetter_sm1);
+	}
+	
+	public static void path(final int number, Sprite sprite, float x1, float y1, float x2, float y2, float x3, float y3)
+	{
+		final Path path = new Path(3).to(x1, y1).to(x2, y2)
+				.to(x3, y3); 
+		sprite.registerEntityModifier(new PathModifier((float) 4.1, path, null, new IPathModifierListener() 
+		{
+			@Override
+			public void onPathStarted(final PathModifier pPathModifier, final IEntity pEntity) 
+			{
+				//Debug.d("onPathStarted");
+			}
+
+			@Override
+			public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex)
+			{
+				//Debug.d("onPathWaypointStarted:  " + pWaypointIndex);
+				
+			}
+
+			@Override
+			public void onPathWaypointFinished(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex)
+			{
+				//Debug.d("onPathWaypointFinished: " + pWaypointIndex);
+			}
+
+			@Override
+			public void onPathFinished(final PathModifier pPathModifier, final IEntity pEntity)
+			{
+				//Debug.d("onPathFinished");
+				MainActivity.parrotWay = number; 
+				
+				if(MainActivity.parrotWay == 14)
+				{
+					Functions.path(15, MainActivity.parrot, MainActivity.letter1Pos,
+							MainActivity.parrotHeight,
+							MainActivity.letter2Pos, MainActivity.letter2.getY(),
+							MainActivity.letter2Pos, MainActivity.parrotHeight
+							);
+				}
+				else if(MainActivity.parrotWay == 13)
+				{
+					Functions.path(14, MainActivity.parrot, -200, MainActivity.parrotHeight,
+							MainActivity.letter1Pos, MainActivity.letter1.getY(),
+							MainActivity.letter1Pos, MainActivity.parrotHeight
+							);
+				}
+				else
+				{
+					MainActivity.parrot.setFlippedHorizontal(false);
+					
+					Functions.path(13, MainActivity.parrot, -200, MainActivity.parrotHeight,
+							MainActivity.letter1Pos, MainActivity.letter1.getY(),
+							MainActivity.letter1Pos, MainActivity.parrotHeight
+							);
+				}
+				
+			}
+		}, EaseSineInOut.getInstance()));
+	}
+	
+	//Audio play Function
+	public static void playAudio(int val)
+	{
+		if(audioPlay)
+		{
+			if(!mediaPlayer.isPlaying())
+			{
+				mediaPlayer.reset();
+				mediaPlayer = MediaPlayer.create(MainActivity.MainActivityInstace, val);
+				
+				try 
+				{
+					mediaPlayer.start();
+					mediaPlayer.setLooping(false);
+				} 
+				catch (IllegalStateException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			audioPlay = true;
+		}
 	}
 }
--- src/com/example/tutorial/MainActivity.java
+++ src/com/example/tutorial/MainActivity.java
@@ -8,9 +8,7 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -20,11 +18,11 @@ import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtla
 import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
-import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.color.Color;
 import org.andengine.util.debug.Debug;
+
 import android.view.Display;
 
 public class MainActivity extends SimpleBaseGameActivity
@@ -39,7 +37,7 @@ public class MainActivity extends SimpleBaseGameActivity
 	public static ITextureRegion mKolomTextureRegion, mBoardTextureRegion;
 	ITiledTextureRegion mParrotTextureRegion;
 	public static ITextureRegion mMoTextureRegion;
-	public static ITextureRegion mLetter1TextureRegion, mLetter2TextureRegion, mLetter3TextureRegion;
+	public static ITextureRegion mLetter1TextureRegion, mLetter2TextureRegion, mLetter3TextureRegion, mLetter4TextureRegion;
 	
 	public static ITextureRegion mbackGroundTextureRegion, mbackGround2TextureRegion;
 	
@@ -48,24 +46,25 @@ public class MainActivity extends SimpleBaseGameActivity
 	public static Sprite backGround, backGround2;
 	public static Sprite board, monkey;
 	public static Parrot parrot; 
-	public static Letter letter1, letter2, letter3;
+	public static Letter letter1, letter2, letter3, letter4;
 	public static Sprite mo;
 	
 	public static MainActivity MainActivityInstace;
 	public static VertexBufferObjectManager vertexBufferObjectManager;
 	
-	public static float parrotHeight, letter1Pos, letter2Pos, letter3Pos;
+	public static float parrotHeight, letter1Pos, letter2Pos, letter3Pos, letter4Pos;
 	public static int parrotWay;
 	
 	public static MainActivity getSharedInstances()
 	{
-		return MainActivityInstace;
+		return MainActivityInstace; 
 	}
 	
 	@Override
 	public EngineOptions onCreateEngineOptions() 
 	{
 		// TODO Auto-generated method stub
+		MainActivityInstace = this;
 		Display display = getWindowManager().getDefaultDisplay();
 		CAMERA_HEIGHT = display.getHeight();
 		CAMERA_WIDTH = display.getWidth();
@@ -74,6 +73,7 @@ public class MainActivity extends SimpleBaseGameActivity
 		letter1Pos = MainActivity.CAMERA_WIDTH/2 - MainActivity.CAMERA_WIDTH/4;
 		letter2Pos = letter1Pos + 100;
 		letter3Pos = letter2Pos + 200;
+		letter4Pos = letter3Pos + 100;
 		
 		mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		
@@ -105,6 +105,8 @@ public class MainActivity extends SimpleBaseGameActivity
 		mLetter2TextureRegion = BitmapTextureAtlasTextureRegionFactory
 				.createFromAsset(this.mBitmapTextureAtlas1, this, "mo.png");
 		mLetter3TextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapTextureAtlas1, this, "mo.png");
+		mLetter4TextureRegion = BitmapTextureAtlasTextureRegionFactory
 				.createFromAsset(this.mBitmapTextureAtlas1, this, "mo.png");
 		
 		mAnimatedBitmapTextureAtlas = new BuildableBitmapTextureAtlas(this.getTextureManager(), 1000, 334, TextureOptions.NEAREST);
@@ -173,15 +175,10 @@ public class MainActivity extends SimpleBaseGameActivity
 		mScene.registerTouchArea(letter3);
 		mScene.attachChild(letter3);
 		
-//		letter2 = new Letter(MainActivity.CAMERA_WIDTH / 18,MainActivity.CAMERA_HEIGHT / 18, MainActivity.mMoTextureRegion,
-//				MainActivity.vertexBufferObjectManager);
-//		mScene.registerTouchArea(letter2);
-//		mScene.attachChild(letter2);
-//		
-//		letter3 = new Letter(MainActivity.CAMERA_WIDTH / 18-100,MainActivity.CAMERA_HEIGHT / 18, MainActivity.mMoTextureRegion,
-//				MainActivity.vertexBufferObjectManager); 
-//		mScene.registerTouchArea(letter3); 
-//		mScene.attachChild(letter3);
+		letter4 = new Letter(MainActivity.CAMERA_WIDTH +100,MainActivity.CAMERA_HEIGHT / 18, MainActivity.mMoTextureRegion,
+				MainActivity.vertexBufferObjectManager); 
+		mScene.registerTouchArea(letter4);
+		mScene.attachChild(letter4);
 		
 		parrot = new Parrot(20, 30, mParrotTextureRegion, getVertexBufferObjectManager());
 		parrot.animate(new long[]{80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80}, 0, 11, true);
@@ -189,84 +186,115 @@ public class MainActivity extends SimpleBaseGameActivity
 		mScene.registerTouchArea(parrot); 
 		mScene.attachChild(parrot);
 		
-		mScene.registerUpdateHandler(new TimerHandler(1/100f, true, new ITimerCallback()
-		{
-			@Override
-			public void onTimePassed(TimerHandler pTimerHandler) 
-			{
-				// TODO Auto-generated method stub
-				if(parrotWay == 1)
-				{
-					Functions.letter(2, 1, MainActivity.letter1, letter1Pos +20, letter1Pos +20,
-							MainActivity.CAMERA_HEIGHT / 2 -80, 
-							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
-				}
-				else if(parrotWay == 2)
-				{
-					Functions.parrotPath(3, 2, parrot, MainActivity.letter1Pos,
-							-150,
-							MainActivity.parrotHeight,
-							MainActivity.parrotHeight
-							);
-				}
-				else if(parrotWay == 3)
-				{ 
-					Functions.parrotWithLetterPath(4, 
-							1, 4, MainActivity.letter2, parrot, -250, letter2Pos, 
-							MainActivity.parrotHeight, MainActivity.parrotHeight);
-				}
-				else if(parrotWay == 4)
-				{
-					Functions.letter(5, 2, MainActivity.letter2, letter2Pos, letter2Pos,
-							MainActivity.CAMERA_HEIGHT / 2 -80, 
-							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
-				} 
-				else if(parrotWay == 5)
-				{
-					Functions.parrotPath(6, 2, parrot, MainActivity.letter2Pos,
-							MainActivity.CAMERA_WIDTH,
-							MainActivity.parrotHeight,
-							MainActivity.parrotHeight
-							);
-				}
-				else if(parrotWay == 6)
-				{
-					Functions.parrotWithLetterPath(7, 
-							0, 2, MainActivity.letter3, parrot, CAMERA_WIDTH, letter3Pos, 
-							MainActivity.parrotHeight, MainActivity.parrotHeight);
-				}
-				else if(parrotWay == 7)
-				{
-					Functions.letter(8, 1, MainActivity.letter3, letter3Pos, letter3Pos,
-							MainActivity.CAMERA_HEIGHT / 2 -80, 
-							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
-				}
-				else if(parrotWay == 8)
-				{
-					Functions.parrotPath(9, 2, parrot, letter3Pos,
-							-150,
-							MainActivity.parrotHeight,
-							MainActivity.parrotHeight
-							);
-
-				}
-				else if(parrotWay == 9)
-				{
-					
-				}
-				else if(parrotWay == 10)
-				{
-					
-				}
-				else if(parrotWay == 11)
-				{
-					
-				}
-			}
-		}));
+//		mScene.registerUpdateHandler(new TimerHandler((float)1.0f/120, true, new ITimerCallback()
+//		{
+//			@Override
+//			public void onTimePassed(TimerHandler pTimerHandler) 
+//			{
+//				// TODO Auto-generated method stub
+//				if(parrotWay == 1)
+//				{
+//					Functions.letter(2, 1, MainActivity.letter1, letter1Pos +20, letter1Pos +20,
+//							MainActivity.CAMERA_HEIGHT / 2 -80, 
+//							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+//				}
+//				else if(parrotWay == 2) 
+//				{
+//					Functions.parrotPath(3, 2, parrot, MainActivity.letter1Pos,
+//							-400, 
+//							MainActivity.parrotHeight,
+//							MainActivity.parrotHeight
+//							);
+//				}
+//				else if(parrotWay == 3)
+//				{ 
+//					Functions.parrotWithLetterPath(4, 
+//							1, 4, MainActivity.letter2, parrot, -400, letter2Pos, 
+//							MainActivity.parrotHeight, MainActivity.parrotHeight);
+//				}
+//				else if(parrotWay == 4)
+//				{
+//					Functions.letter(5, 2, MainActivity.letter2, letter2Pos, letter2Pos,
+//							MainActivity.CAMERA_HEIGHT / 2 -80, 
+//							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+//				} 
+//				else if(parrotWay == 5)
+//				{
+//					Functions.parrotPath(6, 3, parrot, MainActivity.letter2Pos,
+//							MainActivity.CAMERA_WIDTH+400,
+//							MainActivity.parrotHeight,
+//							MainActivity.parrotHeight
+//							);
+//				}
+//				else if(parrotWay == 6)
+//				{
+//					Functions.parrotWithLetterPath(7, 
+//							0, 4, MainActivity.letter3, parrot, CAMERA_WIDTH+400, letter3Pos, 
+//							MainActivity.parrotHeight, MainActivity.parrotHeight);
+//				}
+//				else if(parrotWay == 7)
+//				{
+//					Functions.letter(8, 1, MainActivity.letter3, letter3Pos, letter3Pos,
+//							MainActivity.CAMERA_HEIGHT / 2 -80, 
+//							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+//				}
+//				else if(parrotWay == 8)
+//				{
+//					Functions.parrotPath(9, 4, parrot, letter3Pos,
+//							-400,
+//							MainActivity.parrotHeight,
+//							MainActivity.parrotHeight
+//							);
+//				}
+//				else if(parrotWay == 9)
+//				{
+//					Functions.parrotWithLetterPath(10,
+//							1, 7, MainActivity.letter4, parrot, -600, letter4Pos, 
+//							MainActivity.parrotHeight, MainActivity.parrotHeight);
+//				}
+//				else if(parrotWay == 10)
+//				{
+//					Functions.letter(11, 1, MainActivity.letter4, letter4Pos, letter4Pos,
+//							MainActivity.CAMERA_HEIGHT / 2 -80, 
+//							MainActivity.CAMERA_HEIGHT-MainActivity.CAMERA_HEIGHT/5);
+//				}
+//				else if(parrotWay == 11)
+//				{
+//					parrot.setFlippedHorizontal(true);
+//					Functions.path(12, parrot, MainActivity.letter4Pos, MainActivity.parrotHeight,
+//							MainActivity.letter1Pos, MainActivity.letter1.getY(),
+//							-200, MainActivity.parrotHeight
+//							);
+//				}
+//				else if(parrotWay == 12)
+//				{
+//					parrot.setFlippedHorizontal(false);
+//					Functions.path(13, parrot, -200, MainActivity.parrotHeight,
+//							MainActivity.letter1Pos, MainActivity.letter1.getY(),
+//							MainActivity.letter1Pos, MainActivity.parrotHeight
+//							);
+//				}
+//				else if(parrotWay == 13)
+//				{
+//					
+//				}
+//				else if(parrotWay == 14)
+//				{
+//					
+//				}
+//				else if(parrotWay == 15)
+//				{
+//					
+//				}
+//			}
+//		}));
 		
-		Functions.parrotWithLetterPath(1, 0, 4,  letter1, parrot, MainActivity.CAMERA_WIDTH  , letter1Pos,
+		Functions.audioPlay = true;
+		Functions.playAudio(R.raw.first);
+		
+		Functions.parrotWithLetterPath(1, 0, 6,  letter1, parrot, MainActivity.CAMERA_WIDTH  , letter1Pos,
 				parrotHeight, parrotHeight);
+		
 		
 		return mScene;
 	}
